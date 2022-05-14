469ab0cb54f8c4e9bf800ac6b104ef47487400fa
monojit.laha@gmail.com <monojit.laha@gmail.com>
2022-05-14 12:50:36 +0530

    Added Logging

--- SkillTrackerAPIGateway/Program.cs
+++ SkillTrackerAPIGateway/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace StockMarketAPIGateway
 {
@@ -17,9 +18,10 @@ namespace StockMarketAPIGateway
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-           .ConfigureAppConfiguration((hostingContext, config) =>
-           {
-               config.AddJsonFile("oscelot.json");
-           });
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config.AddJsonFile("ocelot.json");
+               })
+              .ConfigureLogging(logging => logging.AddConsole());
     }
 }
