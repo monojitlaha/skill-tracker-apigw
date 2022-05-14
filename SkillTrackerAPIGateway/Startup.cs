a9a32fe2629dac4f5b707f3a1200d095d79457be
monojit.laha@gmail.com <monojit.laha@gmail.com>
2022-05-14 17:29:20 +0530

    Added Authentication

--- SkillTrackerAPIGateway/Startup.cs
+++ SkillTrackerAPIGateway/Startup.cs
@@ -1,7 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -19,6 +22,24 @@ namespace StockMarketAPIGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var secret = "Skilltrackerprivatekey";
+            var key = Encoding.ASCII.GetBytes(secret);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
             services.AddOcelot(Configuration);
         }
 
@@ -27,6 +48,7 @@ namespace StockMarketAPIGateway
         {
             app.UseRouting();
             app.UseOcelot().Wait();
+            app.UseAuthentication();
         }
     }
 }
